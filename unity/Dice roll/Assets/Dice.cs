a15ec9ad89fc99329eaff21fa72dc40e11749385
Atral <jack_maskell@hotmail.co.uk>
2020-06-23 17:23:38 +0100

    Dice now has arena to roll in. Dice rolls randomly when space is pressed

---
+++ unity/Dice roll/Assets/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    Rigidbody rb;
+    
+    bool hasLanded;
+    bool thrown;
+
+    Vector3 initPos;
+
+    public int diceValue;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initPos = transform.position;
+        rb.useGravity = false;
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            RollDice();
+        }
+    }
+    void RollDice()
+    {
+        if(!thrown && !hasLanded)
+        {
+            thrown = true;
+            rb.useGravity = true;
+            rb.AddTorque(Random.Range(0, 500),Random.Range(0,500), Random.Range(0,500));
+
+        }
+    }
+}
